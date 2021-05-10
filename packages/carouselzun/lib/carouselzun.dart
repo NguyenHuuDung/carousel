4a80e76b5b9749fd27b6282a924891e59e319fca
DESKTOP-JP6EMFA\Dungnh <tb.dungnh@gmail.com>
2021-05-10 17:27:18 +0700

    create

---
+++ packages/carouselzun/lib/carouselzun.dart
@@ -0,0 +1,11 @@
+library carouselzun;
+
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+part 'utils.dart';
+part 'carousel_controller.dart';
+part 'carousel_options.dart';
+part 'carousel_slider.dart';
+part 'carousel_state.dart';
